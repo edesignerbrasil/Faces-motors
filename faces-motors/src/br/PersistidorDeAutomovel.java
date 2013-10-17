078f96e394e05542164f5066260794a70bc778dc
Charles Eduardo <charles.eduardo.br@hotmail.com>
2013-10-16 22:54:12 -0300

    2.6 Consultas simples no banco de dados com a JPA

--- faces-motors/src/br/PersistidorDeAutomovel.java
+++ faces-motors/src/br/PersistidorDeAutomovel.java
@@ -1,9 +1,12 @@
 package br;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import br.com.casadocodigo.jsfjpa.entities.Automovel;
 import br.com.casadocodigo.jsfjpa.persistence.JPAUtil;
@@ -18,16 +21,24 @@ public class PersistidorDeAutomovel {
 				
 		EntityManager em = JPAUtil.getEntityManager();
 		
+		/**
 		Automovel auto = new Automovel();
 		auto.setAnoFabricacao(2011);
 		auto.setModelo("Ferrari Super");
 		auto.setObservacoes("Nunca foi batido");
+		**/
 		
+		Query q = em.createQuery("select a from Automovel a", Automovel.class);
 		
+		List<Automovel> autos = q.getResultList();
+		
+		for(Automovel a : autos){
+			System.out.println(a.getModelo());
+		}
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		
-		em.persist(auto);
+		//em.persist(auto);
 		
 		tx.commit();
 		

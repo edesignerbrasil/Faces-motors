6ab18a4dfd6bec926030f5a23df5095aa1833174
Charles Eduardo <charles.eduardo.br@hotmail.com>
2013-10-17 15:04:29 -0300

    3.4 Pae dados da tela para o Managed Bean 2/7

--- faces-motors/src/br/com/casadocodigo/jsfjpa/controllers/AutomovelBean.java
+++ faces-motors/src/br/com/casadocodigo/jsfjpa/controllers/AutomovelBean.java
@@ -5,7 +5,75 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class AutomovelBean {
 
+	private String marca;
+	private String modelo;
+	private Integer anoFabricacao;
+	private Integer anoModelo;
+	private String observacoes;
+	
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+
+	public Integer getAnoFabricacao() {
+		return anoFabricacao;
+	}
+
+
+
+	public void setAnoFabricacao(Integer anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+
+
+
+	public Integer getAnoModelo() {
+		return anoModelo;
+	}
+
+
+
+	public void setAnoModelo(Integer anoModelo) {
+		this.anoModelo = anoModelo;
+	}
+
+
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+
+
 	public void salva(){
-		
+		System.out.println("entrei salva!!!");
 	}
 }

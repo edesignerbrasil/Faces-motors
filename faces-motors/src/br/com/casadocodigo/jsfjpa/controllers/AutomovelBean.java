08e1699a89089d999064e4aaec10a33e6309be7d
Charles Eduardo <charles.eduardo.br@hotmail.com>
2013-10-17 21:13:45 -0300

    3.7 Liste os dados com o dataTable

--- faces-motors/src/br/com/casadocodigo/jsfjpa/controllers/AutomovelBean.java
+++ faces-motors/src/br/com/casadocodigo/jsfjpa/controllers/AutomovelBean.java
@@ -1,7 +1,10 @@
 package br.com.casadocodigo.jsfjpa.controllers;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import br.com.casadocodigo.jsfjpa.entities.Automovel;
 import br.com.casadocodigo.jsfjpa.persistence.JPAUtil;
@@ -11,8 +14,18 @@ public class AutomovelBean {
 
 	private Automovel automovel = new Automovel();
 	
+	private List<Automovel> automoveis;
+	
 	public Automovel getAutomovel(){
 		return this.automovel;
+	}
+	
+	public List<Automovel> getAutomoveis(){
+		EntityManager em = JPAUtil.getEntityManager();
+		Query q = em.createQuery("select a from Automovel a", Automovel.class);
+		this.automoveis = q.getResultList();
+		em.close();
+		return automoveis;
 	}
 
 	public void salva(Automovel automovel){

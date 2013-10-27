1e59dbeb7a221f8c997ed662b6271dcde84655ea
Charles Eduardo <charles.eduardo.br@hotmail.com>
2013-10-27 19:41:30 -0200

    3.8 Mas minha listagem está executando varias consultas no bd

--- faces-motors/src/br/com/casadocodigo/jsfjpa/controllers/AutomovelBean.java
+++ faces-motors/src/br/com/casadocodigo/jsfjpa/controllers/AutomovelBean.java
@@ -13,22 +13,26 @@ import br.com.casadocodigo.jsfjpa.persistence.JPAUtil;
 public class AutomovelBean {
 
 	private Automovel automovel = new Automovel();
-	
+
 	private List<Automovel> automoveis;
-	
-	public Automovel getAutomovel(){
+
+	public Automovel getAutomovel() {
 		return this.automovel;
 	}
-	
-	public List<Automovel> getAutomoveis(){
-		EntityManager em = JPAUtil.getEntityManager();
-		Query q = em.createQuery("select a from Automovel a", Automovel.class);
-		this.automoveis = q.getResultList();
-		em.close();
+
+	public List<Automovel> getAutomoveis() {
+
+		if (this.automoveis == null) {
+			EntityManager em = JPAUtil.getEntityManager();
+			Query q = em.createQuery("select a from Automovel a",
+					Automovel.class);
+			this.automoveis = q.getResultList();
+			em.close();
+		}
 		return automoveis;
 	}
 
-	public void salva(Automovel automovel){
+	public void salva(Automovel automovel) {
 		EntityManager em = JPAUtil.getEntityManager();
 		em.getTransaction().begin();
 		em.persist(automovel);
